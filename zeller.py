0ba784ec48fe8295f31d9d9b9a8afec4bd134322
ThatArekkusu <arekkusu5665@gmail.com>
2025-02-07 16:02:33 +0000

    V1 of python zeller programme

--- zeller.py
+++ zeller.py
@@ -20,45 +20,53 @@ def zeller():
 
     print("Welcome to the Zeller Programme,\n Input the date of your birth in the order prompted and it will compute the day of the week you were born on!")
 
-    useryear = int(input("What year were you born on?:"))
-    if useryear != len(4):
-        print("Error, Incorrect format, make sure youe year is in the format YYYY")
-        exit()
+    userYear = input("\nWhat year were you born on?: ")
+    if len(userYear) != 4:
+        print("\nError, Incorrect format, make sure your year is in the format YYYY\n")
+    else:
+        try:
+            userYear = int(userYear)
+        except ValueError:
+            print("\nError, Incorrect format, make sure your year is in the format YYYY\n")
         # Error handling for year formatting
 
-    usermonth = str(input("What month were you born on?: "))
-    if usermonth not in months:
-        print("Error, Incorrect format, make sure the first letter is captial and in the format MM\n")
-        exit()
+    userMonth = input("\nWhat month were you born on?: ")
+    if userMonth not in months:
+        print("\nError, Incorrect format, make sure the first letter is captial and in the format MM\n")
+        return()
         # Error handling for month formatting
 
-    userdays = str(input("Input the day you were born on. Ex. 02, Ensure to put in 2 digit format: "))
-    if userdays not in days:
-        print("Error, Incorrect format, make sure your day is in the format DD\n")
-        exit()
+    userDays = input("\nInput the day you were born on. Ex. 02, Ensure to put in 2 digit format: ")
+    if userDays not in days:
+        print("\nError, Incorrect format, make sure your day is in the format DD\n")
+    else:
+        try:
+            userDays = int(userDays)
+        except ValueError:
+            print("\nError, Incorrect format, make sure your day is in the format DD\n")
         # Error handling for number formatting
 
-    det = int(input("Would you like to add additional information? 1 for yes, 2 for no: "))
+    det = int(input("\nWould you like to add additional information? 1 for yes, 2 for no: "))
     if det == 1:
-        additionalinfo()
-    if det == 2:
-        zelleralgo(useryear, usermonth, userdays, months, days)
+        additionalinfo(det, userYear, userMonth, userDays, months)
+    elif det == 2:
+        zelleralgo(det, userYear, userMonth, userDays, months, birthplace="")
     
 
 
-def additionalinfo():
-    birthplace = str(input("Where where you born?"))
+def additionalinfo(det, userYear, userMonth, userDays, months):
+    birthplace = input("\nWhere where you born?: ")
     if birthplace == "":
-        print("Error please enter a location")
-        additionalinfo()
+        print("\nError please enter a location")
+        return()
         # anticipates user not putting in an input and restarts the function if this happens
     else:
-        zelleralgo()
+        zelleralgo(det, userYear, userMonth, userDays, months, birthplace)
             
-def zelleralgo(usermonth, userdays, months, days, birthplace, useryear):
+def zelleralgo(det, userYear, userMonth, userDays, months, birthplace):
     daysindex = ["Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
 
-    x = int((months.index(usermonth)) + 1)
+    x = int((months.index(userMonth)) + 1)
     # references the index to parse the month to an integer
     if x == 1:
         x += 12
@@ -66,10 +74,20 @@ def zelleralgo(usermonth, userdays, months, days, birthplace, useryear):
         x += 12
     # accounts for january and feburary needing to be 13 & 14 in zeller algorithm
 
-    y = userdays
+    X = userDays  # Day of the month
+    Y = userYear % 100  # Year within the century
+    Z = userYear // 100  # Century
 
-    z = float(useryear / 100)
+    # Zeller's formula
+    h = (X + ((13 * (x + 1)) // 5) + Y + (Y // 4) + (Z // 4) + (5 * Z)) % 7
 
     
+    msg = (f"\nYou were born on a {daysindex[h]}")
+    if det == 1:
+        msg += (f" in {birthplace}")
+
+    msg += "."
+
+    print(msg) 
 
 zeller()
